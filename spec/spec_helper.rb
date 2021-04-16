f3ddf3ddc8771dee346264bbc402b95f112b9e97
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-16 14:52:00 +0900

    Factory_rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -30,7 +31,6 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
 end
 
   # rspec-mocks config goes here. You can use an alternate test double
