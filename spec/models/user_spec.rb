6cfe69f403fcdc9d21264736d72451fce6b1aca0
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-17 22:23:32 +0900

    Rspecおおまか

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,55 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    context "データが条件を満たすとき" do
+      it "保存できる" do
+      end
+    end
+    context "name が空のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "name が31文字以上のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "age が空のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "age が文字列のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "age が151以上のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "age が負の整数のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "email が空のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "email が256文字以上のとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "email がすでに存在するとき" do
+      it "エラーが発生する" do
+      end
+    end
+    context "email が アルファベット･英数字 のみのとき" do
+      it "エラーが発生する" do
+      end
+    end
+  end
+
+  context "ユーザーが削除されたとき" do
+    it "そのユーザーのメッセージも削除される" do
+    end
+  end
 end
