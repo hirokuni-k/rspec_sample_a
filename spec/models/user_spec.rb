594d85722294332226e3154023e06a9e2f649fe7
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-17 22:54:22 +0900

    バリデーションテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe User, type: :model do
   describe "バリデーション" do
     context "データが条件を満たすとき" do
       it "保存できる" do
+        user = build(:user)
+        expect(user.valid?).to eq true
       end
     end
     context "name が空のとき" do
       it "エラーが発生する" do
+        user = build(:user, name: "")
+        expect(user.valid?).to eq false
+        expect(user.errors.messages[:name]).to include "を入力してください"
       end
     end
     context "name が31文字以上のとき" do
